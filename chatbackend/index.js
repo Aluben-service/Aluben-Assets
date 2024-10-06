69be4d3ef992dff027e76c7f6dd54dbe92ff178b
I don’t codddde <120886940+asc2563@users.noreply.github.com>
2024-10-06 10:52:38 -0700

    fix this typescript code

--- chatbackend/index.js
+++ chatbackend/index.js
@@ -1,19 +1,32 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const CHAT_EVENT = "chat message";
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log(`Client connected: ${socket.id}`);
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on(CHAT_EVENT, (msg) => {
+    if (msg.username && msg.text) {
+      io.emit(CHAT_EVENT, {
+        username: msg.username.slice(0, 50), // Limit username length
+        text: msg.text.slice(0, 500), // Limit message length
+      });
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log(`Client disconnected: ${socket.id}`);
   });
 });
 
